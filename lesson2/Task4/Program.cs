84f9d5ea9acb01f18f7c91762876266706d688c0
Evgeniy Ryakovskiy <ryakhovskii87@gmail.com>
2024-06-17 17:45:46 -0300

    Решили задачу разными циклами

--- lesson2/Task4/Program.cs
+++ lesson2/Task4/Program.cs
@@ -10,6 +10,8 @@ int[] array = { 2, 4, 7, 3, 9 };
 int i = 0;
 int max = array[0];
 
+//Цикл While
+
 while(i < n)
 {
     if(array[i] > max)
@@ -20,3 +22,29 @@ while(i < n)
 
 }
 Console.WriteLine(max);
+
+//Цпкл  FOR
+
+max = array[0];
+for(int j = 0; j < n; j++)//j = j + 1)
+{
+   if(array[j] > max)
+    {
+        max = array[j];
+    }
+     
+}
+Console.WriteLine(max);
+
+//цикл FOREACH
+
+max = array[0];
+foreach(int e in array)
+{
+    if(e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
+
